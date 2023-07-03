1adbafa854cad1d3b8c2a29e2169e0eb8fe76a03
MaksymStepanenko <113137299+MaksymStepanenko@users.noreply.github.com>
2023-07-03 14:37:17 +0300

    new

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+
+
+function Notification({ message }) {
+  return (
+    <div>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+Notification.prototype = {
+  message: PropTypes.string.isRequired,
+};
+
+export default Notification;
